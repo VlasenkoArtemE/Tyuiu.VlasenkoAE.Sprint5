f018c6d642e539bbb3c4b09a2bebeedc4434aa20
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-11-24 20:46:34 +0500

    Add project files.

---
+++ Tyuiu.VlasenkoAE.Sprint5.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VlasenkoAE.Sprint5.Task0.V27.Lib
+{
+    public class DataService : ISprint5Task0V27
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double y = Math.Pow((x - 1), (3 * x + 1));
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VlasenkoAE.Sprint5.Task0.V27.Lib;
+
+using System.IO;
+
+namespace Tyuiu.VlasenkoAE.Sprint5.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\artem\AppData\Local\Temp\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
