49ee00e661dd4f127b971b6bbd365f2dcf5d2a36
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-11-26 23:00:12 +0500

    Task2

---
+++ Tyuiu.VlasenkoAE.Sprint5.Task2.V3.Lib/DataService.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VlasenkoAE.Sprint5.Task2.V3.Lib
+{
+    public class DataService : ISprint5Task2V3
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            int rows = matrix.GetUpperBound(0) + 1;   // количество строк
+            int columns = matrix.Length / rows;       // количество столбцов
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            string str = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + matrix[i, j] + ";";   // если разделитель это ";", то файл должен быть с расширением .csv 
+                    }
+                    else
+                    {
+                        str = str + matrix[i, j];
+
+                    }
+                }
+
+                if (i != rows - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+
+                str = "";
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint5.Task2.V3/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.VlasenkoAE.Sprint5.Task2.V3.Lib;
+
+int[,] mtrx = new int[3, 3] { { 5, 9, 1 },
+                              { 1, 3, 9 },
+                              { 1, 2, 2 } } ;
+
+int rows = mtrx.GetUpperBound(0) + 1;   // количество строк
+int columns = mtrx.Length / rows;       // количество столбцов
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");    //write для того, чтобы вывод был корректным
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(mtrx);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
